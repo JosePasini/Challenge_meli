7149f95eb9144aa7ea2238ee58ef97733032beb2
JosePasini <josepasini.17@gmail.com>
2021-09-26 11:02:06 -0300

    intentando deploy heroku

--- Challenge_meli/meli_challenge/src/main/java/com/example/meli_punto_dos/entities/AdnCadena.java
+++ Challenge_meli/meli_challenge/src/main/java/com/example/meli_punto_dos/entities/AdnCadena.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class AdnCadena {
+public class AdnCadena implements Serializable {
 
     @JsonProperty("dna")
     private List<String> adn_cadena;
